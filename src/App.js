e84b191b2d32dda4f66923c923b6f2a2527e5ef9
tuan-hda <anhtuan.hoangdinh@gmail.com>
2022-06-05 20:18:07 +0700

    Some changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Product from './pages/Product'
 import User from './pages/User'
 import Transaction from './pages/Transaction'
 import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/signin' element={<SignIn />} />
-      <Route path='/signup' element={<SignUp />} />
       <Route path='/' element={<Main />}>
         <Route path='product' element={<Product />} />
         <Route path='user' element={<User />} />
