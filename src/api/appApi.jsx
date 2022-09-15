8fa2fcbecfdf3396be087450694d1abe857d2630
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2022-09-15 14:59:40 +0700

    update

--- src/api/appApi.jsx
+++ src/api/appApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const appApi = axios.create({
-  baseURL: 'http://localhost:8081/api/'
+  baseURL: 'https://hambursy-server.herokuapp.com/api/'
 })
 
 export default appApi
